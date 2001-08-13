24e0ab2170a06906b8e3ae849b3df670092245b8
desruisseaux <martin.desruisseaux@geomatys.com>
2001-08-13 12:33:21 +0000

    Code for French resources

---
+++ net/seas/resources/Resources_fr.java
@@ -0,0 +1,49 @@
+/*
+ * SEAS - Surveillance de l'Environnement Assistée par Satellites
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation; either
+ *    version 2.1 of the License, or (at your option) any later version.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ * Contacts:
+ *     FRANCE: Surveillance de l'Environnement Assistée par Satellite
+ *             Institut de Recherche pour le Développement
+ *             mailto:seasnet@teledetection.fr
+ *
+ *     CANADA: Observatoire du Saint-Laurent
+ *             Institut Maurice-Lamontagne
+ *             mailto:osl@osl.gc.ca
+ */
+package net.seas.resources;
+
+// Entrés/sorties
+import java.io.IOException;
+
+
+/**
+ * Ressources en langue française.
+ *
+ * @version 1.0
+ * @author Martin Desruisseaux
+ */
+public class Resources_fr extends Resources
+{
+    /**
+     * Nom du fichier dans lequel sont enregistrées les données.
+     */
+    static final String FILEPATH = "net/seas/resources/resources_fr.dat";
+
+    /**
+     * Initialise les ressources françaises.
+     * @throws IOException si les ressources
+     *         n'ont pas pu être ouvertes.
+     */
+    public Resources_fr() throws IOException
+    {super(FILEPATH);}
+}
