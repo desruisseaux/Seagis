b23733b38b9f82a2a4d058efc97687d51f81c7b0
desruisseaux <martin.desruisseaux@geomatys.com>
2001-08-13 12:25:00 +0000

    Added new package

---
+++ net/seas/awt/tree/DefaultMutableTreeNode.java
@@ -0,0 +1,63 @@
+/*
+ * SEAS - Surveillance de l'Environnement Assistée par Satellites
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation; either
+ *    version 2.1 of the License, or (at your option) any later version.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ * Contacts:
+ *     FRANCE: Surveillance de l'Environnement Assistée par Satellite
+ *             Institut de Recherche pour le Développement
+ *             mailto:seasnet@teledetection.fr
+ *
+ *     CANADA: Observatoire du Saint-Laurent
+ *             Institut Maurice-Lamontagne
+ *             mailto:osl@osl.gc.ca
+ */
+package net.seas.awt.tree;
+
+
+/** 
+ * General-purpose node in a tree data structure.
+ *
+ * @version 1.0
+ * @author Martin Desruisseaux
+ */
+public class DefaultMutableTreeNode extends javax.swing.tree.DefaultMutableTreeNode implements MutableTreeNode
+{
+    /**
+     * Creates a tree node that has no parent and no children, but which
+     * allows children.
+     */
+    public DefaultMutableTreeNode()
+    {super();}
+
+    /**
+     * Creates a tree node with no parent, no children, but which allows 
+     * children, and initializes it with the specified user object.
+     * 
+     * @param userObject an Object provided by the user that constitutes
+     *                   the node's data
+     */
+    public DefaultMutableTreeNode(Object userObject)
+    {super(userObject);}
+
+    /**
+     * Creates a tree node with no parent, no children, initialized with
+     * the specified user object, and that allows children only if
+     * specified.
+     * 
+     * @param userObject an Object provided by the user that constitutes
+     *        the node's data
+     * @param allowsChildren if true, the node is allowed to have child
+     *        nodes -- otherwise, it is always a leaf node
+     */
+    public DefaultMutableTreeNode(Object userObject, boolean allowsChildren)
+    {super(userObject, allowsChildren);}
+}
