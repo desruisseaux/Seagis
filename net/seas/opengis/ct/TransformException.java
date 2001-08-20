b38c8f3ffa7be6135126ab47ba25ef4c09009f4c
desruisseaux <martin.desruisseaux@geomatys.com>
2001-08-20 19:05:36 +0000

    Added custom exception

---
+++ net/seas/opengis/ct/TransformException.java
@@ -0,0 +1,51 @@
+/*
+ * OpenGIS implementation in Java
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation; either
+ *    version 2.1 of the License, or (at your option) any later version.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ * Contacts:
+ *     FRANCE: Surveillance de l'Environnement Assistée par Satellite
+ *             Institut de Recherche pour le Développement
+ *             mailto:seasnet@teledetection.fr
+ *
+ *     CANADA: Observatoire du Saint-Laurent
+ *             Institut Maurice-Lamontagne
+ *             mailto:osl@osl.gc.ca
+ */
+package net.seas.opengis.ct;
+
+
+/**
+ * Thrown by {@link MathTransform} when a transformation failed.
+ *
+ * @version 1.0
+ * @author André Gosselin
+ * @author Martin Desruisseaux
+ */
+public class TransformException extends Exception
+{
+    /**
+     * Serial number for compatibility with different versions.
+     */
+    // private static final long serialVersionUID = ?; // TODO
+
+    /**
+     * Constructs a new exception with no detail message.
+     */
+    public TransformException()
+    {}
+
+    /**
+     * Constructs a new exception with the specified detail message.
+     */
+    public TransformException(final String message)
+    {super(message);}
+}
