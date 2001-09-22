d52f9a8916a99c5993e334a0c7e26f1d791c2082
desruisseaux <martin.desruisseaux@geomatys.com>
2001-09-22 17:48:39 +0000

    Added coordinate transform and factory

---
+++ net/seas/opengis/ct/CoordinateTransform.java
@@ -0,0 +1,228 @@
+/*
+ * OpenGIS implementation in Java
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation; either
+ *    version 2.1 of the License, or (at your option) any later version.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ * Contacts:
+ *     FRANCE: Surveillance de l'Environnement Assistée par Satellite
+ *             Institut de Recherche pour le Développement
+ *             mailto:seasnet@teledetection.fr
+ *
+ *     CANADA: Observatoire du Saint-Laurent
+ *             Institut Maurice-Lamontagne
+ *             mailto:osl@osl.gc.ca
+ */
+package net.seas.opengis.ct;
+
+// OpenGIS dependencies
+import org.opengis.ct.CT_TransformType;
+import org.opengis.ct.CT_MathTransform;
+import org.opengis.ct.CT_CoordinateTransformation;
+import org.opengis.cs.CS_CoordinateSystem;
+
+// Coordinates
+import net.seas.opengis.cs.Info;
+import net.seas.opengis.cs.CoordinateSystem;
+
+// Remote Method Invocation
+import java.rmi.RemoteException;
+import java.rmi.server.RemoteObject;
+
+// Miscellaneous
+import java.util.Locale;
+import net.seas.util.XClass;
+
+
+/**
+ * Describes a coordinate transformation. This service class transforms a coordinate point
+ * position between two different coordinate reference systems. A coordinate transformation
+ * interface establishes an association between a source and a target coordinate reference
+ * system, and provides operations for transforming coordinates in the source coordinate
+ * reference system to coordinates in the target coordinate reference system. These coordinate
+ * systems can be ground or image coordinates. In general mathematics, "transformation" is the
+ * general term for mappings between coordinate systems (see tensor analysis).
+ * <br><br>
+ * For a ground coordinate point, if the transformation depends only on mathematically
+ * derived parameters (as in a cartographic projection), then this is an ISO conversion.
+ * If the transformation depends on empirically derived parameters (as in datum
+ * transformations), then this is an ISO transformation.
+ *
+ * @version 1.00
+ * @author OpenGIS (www.opengis.org)
+ * @author Martin Desruisseaux
+ *
+ * @see org.opengis.ct.CT_CoordinateTransformation
+ */
+public abstract class CoordinateTransform extends MathTransform
+{
+    /**
+     * Serial number for interoperability with different versions.
+     */
+    //private static final long serialVersionUID = -5428884291871647867L; // TODO
+
+    /**
+     * Construct a coordinate transformation.
+     *
+     * @param name The coordinate transform name.
+     */
+    public CoordinateTransform(final String name)
+    {super(name);}
+
+    /**
+     * Gets the semantic type of transform.
+     * For example, a datum transformation
+     * or a coordinate conversion.
+     */
+    public abstract TransformType getTransformType();
+
+    /**
+     * Gets the source coordinate system.
+     */
+    public abstract CoordinateSystem getSourceCS();
+
+    /**
+     * Gets the target coordinate system.
+     */
+    public abstract CoordinateSystem getTargetCS();
+    
+    /**
+     * Gets the dimension of input points.
+     */
+    public int getDimSource()
+    {return getSourceCS().getDimension();}
+    
+    /**
+     * Gets the dimension of output points.
+     */
+    public int getDimTarget()
+    {return getTargetCS().getDimension();}
+
+    /**
+     * Returns a hash value for this
+     * coordinate transformation.
+     */
+    public int hashCode()
+    {
+        int code = 7851236;
+        CoordinateSystem cs;
+        if ((cs=getSourceCS()) != null) code = code*37 + cs.hashCode();
+        if ((cs=getTargetCS()) != null) code = code*37 + cs.hashCode();
+        return code;
+    }
+
+    /**
+     * Compares the specified object with this
+     * coordinate transformation for equality.
+     */
+    public boolean equals(final Object object)
+    {
+        if (super.equals(object))
+        {
+            final CoordinateTransform that = (CoordinateTransform) object;
+            return XClass.equals(this.getTransformType(), that.getTransformType()) &&
+                   XClass.equals(this.getSourceCS(),      that.getSourceCS()     ) &&
+                   XClass.equals(this.getTargetCS(),      that.getTargetCS()     );
+        }
+        return false;
+    }
+
+    /**
+     * Returns an OpenGIS interface for this math transform.
+     * The returned object is suitable for RMI use.
+     *
+     * Note: The returned type is a generic {@link Object} in order
+     *       to avoid too early class loading of OpenGIS interface.
+     */
+    Object toOpenGIS()
+    {return new Export();}
+
+
+
+
+    /////////////////////////////////////////////////////////////////////////
+    ////////////////                                         ////////////////
+    ////////////////             OPENGIS ADAPTER             ////////////////
+    ////////////////                                         ////////////////
+    /////////////////////////////////////////////////////////////////////////
+
+    /**
+     * Wrap a {@link CoordinateTransform} for use with OpenGIS.
+     * This wrapper is a good place to check for non-implemented
+     * OpenGIS methods (just check for methods throwing
+     * {@link UnsupportedOperationException}). This class
+     * is suitable for RMI use.
+     *
+     * @version 1.0
+     * @author Martin Desruisseaux
+     */
+    final class Export extends RemoteObject implements CT_CoordinateTransformation
+    {
+        /**
+         * Returns the underlying math transform.
+         */
+        public final CoordinateTransform unwrap()
+        {return CoordinateTransform.this;}
+
+        /**
+         * Name of transformation.
+         */
+        public String getName() throws RemoteException
+        {return CoordinateTransform.this.getName(null);}
+
+        /**
+         * Authority which defined transformation and parameter values.
+         */
+        public String getAuthority() throws RemoteException
+        {return CoordinateTransform.this.getAuthority(null);}
+
+        /**
+         * Code used by authority to identify transformation.
+         */
+        public String getAuthorityCode() throws RemoteException
+        {return CoordinateTransform.this.getAuthorityCode(null);}
+
+        /**
+         * Gets the provider-supplied remarks.
+         */
+        public String getRemarks() throws RemoteException
+        {return CoordinateTransform.this.getRemarks(null);}
+
+        /**
+         * Human readable description of domain in source coordinate system.
+         */
+        public String getAreaOfUse() throws RemoteException
+        {throw new UnsupportedOperationException("Not implemented");}
+
+        /**
+         * Semantic type of transform.
+         */
+        public CT_TransformType getTransformType() throws RemoteException
+        {return new CT_TransformType(CoordinateTransform.this.getTransformType().value);}
+
+        /**
+         * Source coordinate system.
+         */
+        public CS_CoordinateSystem getSourceCS() throws RemoteException
+        {return net.seas.opengis.cs.Adapters.export(CoordinateTransform.this.getSourceCS());}
+
+        /**
+         * Target coordinate system.
+         */
+        public CS_CoordinateSystem getTargetCS() throws RemoteException
+        {return net.seas.opengis.cs.Adapters.export(CoordinateTransform.this.getTargetCS());}
+
+        /**
+         * Gets math transform.
+         */
+        public CT_MathTransform getMathTransform() throws RemoteException
+        {return Adapters.export(CoordinateTransform.this.getMathTransform());}
+    }
+}
---
+++ net/seas/opengis/ct/CoordinateTransformFactory.java
@@ -0,0 +1,160 @@
+/*
+ * OpenGIS implementation in Java
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation; either
+ *    version 2.1 of the License, or (at your option) any later version.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ *
+ * Contacts:
+ *     FRANCE: Surveillance de l'Environnement Assistée par Satellite
+ *             Institut de Recherche pour le Développement
+ *             mailto:seasnet@teledetection.fr
+ *
+ *     CANADA: Observatoire du Saint-Laurent
+ *             Institut Maurice-Lamontagne
+ *             mailto:osl@osl.gc.ca
+ */
+package net.seas.opengis.ct;
+
+// OpenGIS (SEAGIS) dependencies
+import net.seas.opengis.cs.AxisInfo;
+import net.seas.opengis.cs.Projection;
+import net.seas.opengis.cs.AxisOrientation;
+import net.seas.opengis.cs.CoordinateSystem;
+import net.seas.opengis.cs.ProjectedCoordinateSystem;
+import net.seas.opengis.cs.GeographicCoordinateSystem;
+
+// Miscellaneous
+import javax.units.Unit;
+
+
+/**
+ * Creates coordinate transformations.
+ *
+ * @version 1.0
+ * @author OpenGIS (www.opengis.org)
+ * @author Martin Desruisseaux
+ */
+public class CoordinateTransformFactory
+{
+    /**
+     * The default coordinate transformation factory.
+     */
+    public static final CoordinateTransformFactory DEFAULT = new CoordinateTransformFactory(MathTransformFactory.DEFAULT);
+
+    /**
+     * The math transform factory to use.
+     */
+    private MathTransformFactory factory;
+
+    /**
+     * Construct a coordinate transformation factory.
+     *
+     * @param factory The math transform factory to use.
+     */
+    public CoordinateTransformFactory(final MathTransformFactory factory)
+    {this.factory = factory;}
+
+    /**
+     * Creates a transformation between two coordinate systems.
+     * This method will examine the coordinate systems in order to
+     * construct a transformation between them. This method may fail if no
+     * path between the coordinate systems is found.
+     *
+     * @param sourceCS Input coordinate system.
+     * @param targetCS Output coordinate system.
+     */
+    public CoordinateTransform createFromCoordinateSystems(final CoordinateSystem sourceCS, final CoordinateSystem targetCS)
+    {
+        if (sourceCS instanceof GeographicCoordinateSystem)
+        {
+            final GeographicCoordinateSystem source = (GeographicCoordinateSystem) sourceCS;
+            if (targetCS instanceof GeographicCoordinateSystem)
+            {
+                return create(source, (GeographicCoordinateSystem) targetCS);
+            }
+            if (targetCS instanceof ProjectedCoordinateSystem)
+            {
+                return create(source, (ProjectedCoordinateSystem) targetCS);
+            }
+        }
+        if (sourceCS instanceof ProjectedCoordinateSystem)
+        {
+            final ProjectedCoordinateSystem source = (ProjectedCoordinateSystem) sourceCS;
+            if (targetCS instanceof GeographicCoordinateSystem)
+            {
+            //  return create(source, (GeographicCoordinateSystem) targetCS);
+            }
+            if (targetCS instanceof ProjectedCoordinateSystem)
+            {
+                return create(source, (ProjectedCoordinateSystem) targetCS);
+            }
+        }
+        throw new UnsupportedOperationException(); // TODO
+    }
+
+    /**
+     * Creates a transformation between two geographic coordinate systems. This
+     * method is automatically invoked by <code>createFromCoordinateSystems</code>.
+     */
+    protected CoordinateTransform create(final GeographicCoordinateSystem sourceCS, final GeographicCoordinateSystem targetCS)
+    {
+        if (sourceCS.equals(targetCS)) // TODO: "equals" is to severe, since it also check name and remaks.
+        {
+            return new CoordinateTransformProxy(TransformType.CONVERSION, sourceCS, targetCS, AffineTransform2D.IDENTITY);
+        }
+        throw new UnsupportedOperationException("Not implemented");
+    }
+
+    /**
+     * Creates a transformation between two projected coordinate systems. This
+     * method is automatically invoked by <code>createFromCoordinateSystems</code>.
+     */
+    protected CoordinateTransform create(final ProjectedCoordinateSystem sourceCS, final ProjectedCoordinateSystem targetCS)
+    {
+        if (sourceCS.equals(targetCS)) // TODO: "equals" is to severe, since it also check name and remaks.
+        {
+            return new CoordinateTransformProxy(TransformType.CONVERSION, sourceCS, targetCS, AffineTransform2D.IDENTITY);
+        }
+        throw new UnsupportedOperationException("Not implemented");
+    }
+
+    /**
+     * Creates a transformation between a geographic and a projected coordinate systems.
+     * This  method is automatically invoked by <code>createFromCoordinateSystems</code>.
+     */
+    protected CoordinateTransform create(final GeographicCoordinateSystem sourceCS, final ProjectedCoordinateSystem targetCS)
+    {
+        if (!sourceCS.equals(targetCS.getGeographicCoordinateSystem()))
+        {
+            // TODO: call "create(GeographicCS, GeographicCS) instead.
+            throw new UnsupportedOperationException("Not implemented");
+        }
+        if (sourceCS.getPrimeMeridian().getLongitude() != 0                  ||
+            !Unit.DEGREE            .equals(sourceCS.getAngularUnit())       ||
+            !Unit.METRE             .equals(targetCS.getLinearUnit())        ||
+            !AxisOrientation.EAST   .equals(sourceCS.getAxis(0).orientation) ||
+            !AxisOrientation.EAST   .equals(targetCS.getAxis(0).orientation) ||
+            !AxisOrientation.NORTH  .equals(sourceCS.getAxis(1).orientation) ||
+            !AxisOrientation.NORTH  .equals(targetCS.getAxis(1).orientation))
+        {
+            throw new UnsupportedOperationException("Not implemented");
+        }
+        final Projection   projection = targetCS.getProjection();
+        final MathTransform transform = factory.createParameterizedTransform(projection.getClassName(), projection.getParameters());
+        if (transform instanceof MapProjection)
+        {
+            final MapProjection proj = (MapProjection) transform;
+            proj.sourceCS = sourceCS;
+            proj.targetCS = targetCS;
+            return proj;
+        }
+        return new CoordinateTransformProxy(TransformType.CONVERSION, sourceCS, targetCS, transform);
+    }
+}
