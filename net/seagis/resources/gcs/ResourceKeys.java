1918b041bba3a02427934cb95fb81e0dc6e5fc45
desruisseaux <martin.desruisseaux@geomatys.com>
2002-01-04 23:53:20 +0000

    Added resources for Grid Coverage

---
+++ net/seagis/resources/gcs/ResourceKeys.java
@@ -0,0 +1,57 @@
+/*
+ * SEAGIS - An OpenSource implementation of OpenGIS specification
+ *          (C) 2001, Institut de Recherche pour le Développement
+ *
+ *          THIS IS AN AUTOMATICALLY GENERATED FILE. DO NOT EDIT!
+ *          Generated with: net.seagis.resources.ResourceCompiler
+ */
+package net.seagis.resources.gcs;
+
+
+/**
+ * Resource keys. This interface is used when compiling sources, but
+ * no dependencies to <code>ResourceKeys</code> should appear in any
+ * resulting class files.  Since Java compiler inline final integers
+ * values, using long identifiers will not bloat constant pools of
+ * classes compiled against the interface, providing that no class
+ * implements this interface.
+ *
+ * @see net.seagis.resources.ResourceBundle
+ * @see net.seagis.resources.ResourceCompiler
+ */
+public interface ResourceKeys
+{
+    public static final int BLACK                                =     0;
+    public static final int BLUE                                 =     1;
+    public static final int CYAN                                 =     2;
+    public static final int GRAY                                 =     3;
+    public static final int GREEN                                =     4;
+    public static final int HUE                                  =     5;
+    public static final int LIGHTNESS                            =     6;
+    public static final int MAGENTA                              =     7;
+    public static final int NODATA                               =     8;
+    public static final int PALETTE                              =     9;
+    public static final int RED                                  =    10;
+    public static final int SATURATION                           =    11;
+    public static final int TRANSPARENCY                         =    12;
+    public static final int UNDEFINED                            =    13;
+    public static final int YELLOW                               =    14;
+
+    public static final int ERROR_BAD_BAND_NUMBER_$1             =    15;
+    public static final int ERROR_BAD_COEFFICIENT_$2             =    16;
+    public static final int ERROR_BAD_GRID_RANGE_$1              =    17;
+    public static final int ERROR_BAD_RANGE_$2                   =    18;
+    public static final int ERROR_EMPTY_ENVELOPE                 =    19;
+    public static final int ERROR_ENVELOPE_MISMATCH              =    20;
+    public static final int ERROR_INCOMPATIBLE_COORDINATE_SYSTEM =    21;
+    public static final int ERROR_MISMATCHED_ARRAY_LENGTH        =    22;
+    public static final int ERROR_MISMATCHED_DIMENSION_$2        =    23;
+    public static final int ERROR_NUMBER_OF_BANDS_MISMATCH_$2    =    24;
+    public static final int ERROR_OPERATION_ALREADY_BOUND_$1     =    25;
+    public static final int ERROR_OPERATION_NOT_FOUND_$1         =    26;
+    public static final int ERROR_POINT_OUTSIDE_COVERAGE_$1      =    27;
+    public static final int ERROR_RANGE_OVERLAP_$4               =    28;
+    public static final int ERROR_UNKNOW_INTERPOLATION_$1        =    29;
+    public static final int ERROR_UNKNOW_PARAMETER_NAME_$1       =    30;
+    public static final int ERROR_UNSPECIFIED_IMAGE_SIZE         =    31;
+}
