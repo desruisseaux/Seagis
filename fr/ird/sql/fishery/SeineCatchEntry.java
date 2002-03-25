ed821c70264a7ecfa7259d8fdf6ef0b384887a84
desruisseaux <martin.desruisseaux@geomatys.com>
2002-03-25 22:38:09 +0000

    Implementation for Seiner data

---
+++ fr/ird/sql/fishery/SeineCatchEntry.java
@@ -0,0 +1,177 @@
+/*
+ * Remote sensing images: database and visualisation
+ * Copyright (C) 2000 Institut de Recherche pour le Développement
+ *
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Library General Public
+ *    License as published by the Free Software Foundation; either
+ *    version 2 of the License, or (at your option) any later version.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Library General Public License for more details (http://www.gnu.org/).
+ *
+ *
+ * Contact: Michel Petit
+ *          Maison de la télédétection
+ *          Institut de Recherche pour le développement
+ *          500 rue Jean-François Breton
+ *          34093 Montpellier
+ *          France
+ *
+ *          mailto:Michel.Petit@mpl.ird.fr
+ */
+package fr.ird.sql.fishery;
+
+// Base de données
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+// Coordonnées spatio-temporelles
+import java.util.Date;
+import java.awt.Shape;
+import java.awt.geom.Line2D;
+import java.awt.geom.Point2D;
+import java.awt.geom.Rectangle2D;
+import java.awt.geom.RectangularShape;
+import net.seagis.cs.CoordinateSystem;
+import net.seagis.cs.GeographicCoordinateSystem;
+
+// Divers
+import javax.units.Unit;
+import javax.media.jai.util.Range;
+import fr.ird.animat.Species;
+
+
+/**
+ * Données d'une capture à la palangre. Un objet <code>SeineCatchEntry</code>
+ * correspond à une entrée de la table "Captures" de la base de données "Sennes".
+ *
+ * @version 1.0
+ * @author Martin Desruisseaux
+ */
+final class SeineCatchEntry extends AbstractCatchEntry
+{
+    /**
+     * Date et heure de la capture, en nombre de
+     * millisecondes écoulées depuis le 1 janvier 1970.
+     */
+    private final long date;
+
+    /**
+     * Longitude et latitude de la capture, en degrés.
+     */
+    private final float x,y;
+
+    /**
+     * Construit un enregistrement représentant une capture à la senne.
+     *
+     * @param  table  Table d'où proviennent les données.
+     * @param  result Résultat de la requête SQL.
+     * @throws SQLException si l'interrogation de la base de données a échoué.
+     */
+    public SeineCatchEntry(final SeineCatchTable table, final ResultSet result) throws SQLException
+    {
+        super(result.getInt(SeineCatchTable.ID), table.species.species);
+        final float efu;
+
+        date = table.getTimestamp(SeineCatchTable.DATE, result).getTime();
+        x    = getFloat(result,   SeineCatchTable.LONGITUDE);
+        y    = getFloat(result,   SeineCatchTable.LATITUDE );
+
+        for (int i=0; i<amount.length; i++)
+        {
+            amount[i] = getFloat(result, SeineCatchTable.CATCH_AMOUNT + i);
+        }
+    }
+
+    /**
+     * Retourne le nombre réel de la colonne spécifié, ou
+     * <code>NaN</code> si ce nombre réel n'est pas spécifié.
+     */
+    private static float getFloat(final ResultSet result, final int column) throws SQLException
+    {
+        final float value = result.getFloat(column);
+        return result.wasNull() ? Float.NaN : value;
+    }
+
+    /**
+     * Ramène toujours la position à {@link EnvironmentTable#CENTER},
+     * étant donné que la base de données des senneurs ne contient pas
+     * de coordonnées de début et de fin.
+     */
+    final int clampPosition(final int pos)
+    {
+        if (pos<EnvironmentTable.START_POINT || pos>EnvironmentTable.END_POINT)
+        {
+            return pos;
+        }
+        return EnvironmentTable.CENTER;
+    }
+
+    /**
+     * Retourne une coordonnée représentative de la
+     * capture, en degrés de longitude et latitude.
+     */
+    public Point2D getCoordinate()
+    {return new Point2D.Float(x,y);}
+
+    /**
+     * Retourne une forme représentant le filage. Si les informations
+     * disponibles ne permettent pas de connaître le filage, retourne
+     * <code>null</code>.
+     */
+    public Shape getShape()
+    {return null;}
+
+    /**
+     * Verifie si cette capture intercepte le rectangle spécifié.
+     * Cette méthode vérifie simplement si la coordonnées de la
+     * capture se trouve à l'intérieur du rectangle.
+     */
+    public boolean intersects(final Rectangle2D rect)
+    {return rect.contains(x,y);}
+
+    /**
+     * Retourne une date représentative de la pêche. Dans le cas des pêches
+     * qui s'étendent sur une certaine période de temps, ça pourrait être par
+     * exemple la date du milieu.
+     */
+    public Date getTime()
+    {return new Date(date);}
+
+    /**
+     * Retourne la plage de temps pendant laquelle a été faite la capture.
+     * Les éléments de la plage retournée seront du type {@link Date}.
+     */
+    public Range getTimeRange()
+    {
+        final Date date = new Date(this.date);
+        return new Range(Date.class, date, date);
+    }
+
+    /**
+     * Retourne les unités des captures.
+     */
+    public Unit getUnit()
+    {return Unit.TON;}
+
+    /**
+     * Vérifie si cette capture est
+     * identique à l'objet spécifié.
+     */
+    public boolean equals(final Object other)
+    {
+        if (other instanceof SeineCatchEntry)
+        {
+            final SeineCatchEntry that = (SeineCatchEntry) other;
+            return this.ID   == that.ID   &&
+                   this.date == that.date &&
+                   Float.floatToIntBits(this.x) == Float.floatToIntBits(that.x) &&
+                   Float.floatToIntBits(this.y) == Float.floatToIntBits(that.y);
+        }
+        else return false;
+    }
+}
