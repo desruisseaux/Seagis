fc234975d496dc9ef317a48c26ac033009378363
desruisseaux <martin.desruisseaux@geomatys.com>
2002-06-08 22:41:53 +0000

    Refactored the 'animat' implementation

---
+++ fr/ird/animat/seas/AttentiveObject.java
@@ -0,0 +1,54 @@
+/*
+ * Remote sensing images: database and visualisation
+ * Copyright (C) 2002 Institut de Recherche pour le Développement
+ *
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Library General Public
+ *    License as published by the Free Software Foundation; either
+ *    version 2 of the License, or (at your option) any later version.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Library General Public License for more details (http://www.gnu.org/).
+ *
+ *
+ * Contact: Michel Petit
+ *          Maison de la télédétection
+ *          Institut de Recherche pour le développement
+ *          500 rue Jean-François Breton
+ *          34093 Montpellier
+ *          France
+ *
+ *          mailto:Michel.Petit@mpl.ird.fr
+ */
+package fr.ird.animat.seas;
+
+// Interfaces
+import fr.ird.animat.Environment;
+
+
+/**
+ * Objet qui, en plus d'être mobile, est attentif aux signaux de son
+ * environnement.
+ *
+ * @version 1.0
+ * @author Martin Desruisseaux
+ */
+class AttentiveObject extends MobileObject
+{
+    /**
+     * Construit un objet mobile qui n'a pas de position initiale.
+     * Appellez {@link #setLocation} après la construction de cet
+     * objet pour lui affecter une position.
+     */
+    public AttentiveObject()
+    {}
+
+    /**
+     */
+    public void observe(final Environment environment)
+    {
+    }
+}
