ed2759a11d6a21977add9a806bd72b6d8f7c331c
desruisseaux <martin.desruisseaux@geomatys.com>
2002-06-14 13:11:42 +0000

    Moved from 'fr.ird.resources'

---
+++ fr/ird/resources/gui/Resources_fr.java
@@ -0,0 +1,47 @@
+/*
+ * Remote sensing images: database and visualisation
+ * Copyright (C) 2000 Institut de Recherche pour le Développement
+ *
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Library General Public
+ *    License as published by the Free Software Foundation; either
+ *    version 2 of the License, or (at your option) any later version.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Library General Public License for more details (http://www.gnu.org/).
+ *
+ *
+ * Contact: Michel Petit
+ *          Maison de la télédétection
+ *          Institut de Recherche pour le développement
+ *          500 rue Jean-François Breton
+ *          34093 Montpellier
+ *          France
+ *
+ *          mailto:Michel.Petit@mpl.ird.fr
+ */
+package fr.ird.resources.gui;
+
+
+/**
+ * French language resource.
+ *
+ * @version 1.0
+ * @author Martin Desruisseaux
+ */
+public class Resources_fr extends Resources
+{
+    /**
+     * The resource file, using UTF-8 encoding.
+     */
+    static final String FILEPATH = "fr/ird/resources/resources_fr.utf";
+
+    /**
+     * Construct resources for French.
+     */
+    public Resources_fr()
+    {super(FILEPATH);}
+}
