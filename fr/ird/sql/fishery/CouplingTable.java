721d0b78694563f1da448d8a7a808db5860ea6f4
desruisseaux <martin.desruisseaux@geomatys.com>
2002-09-22 21:21:19 +0000

    Changed CouplingTable implementation

--- fr/ird/sql/fishery/CouplingTable.java
+++ fr/ird/sql/fishery/CouplingTable.java
@@ -30,10 +30,11 @@ import java.util.List;
 import java.io.Writer;
 import java.io.IOException;
 import java.sql.SQLException;
-import java.sql.ResultSet;
+import javax.sql.RowSet;
 
 // SEAS dependencies
 import fr.ird.sql.Table;
+import fr.ird.awt.progress.Progress;
 
 
 /**
@@ -47,39 +48,77 @@ import fr.ird.sql.Table;
  * @author Martin Desruisseaux
  */
 public interface CouplingTable extends Table {
+
     /**
-     * Définit les paramètres examinées par cette table. Les paramètres doivent être des
-     * noms de la table "Paramètres". Des exemples de valeurs sont "SST", "CHL", "SLA",
-     * "U", "V" et "EKP".
+     * Oublie tous les paramètres qui ont été déclarés avec {@link #addParameter}.
      *
-     * @param parameter Les paramètres à définir (exemple: "SST").
      * @throws SQLException si l'accès à la base de données a échoué.
      */
-    public void setParameters(final String[] parameters) throws SQLException;
+    public void clear() throws SQLException;
 
     /**
-     * Définit les décalages de temps (en jours).
+     * Ajoute un paramètre à la sélection. Ce paramètre sera pris en compte
+     * lors du prochain appel de la méthode {@link #getRowSet}.
      *
-     * @parm   timeLags Décalages de temps en jours.
+     * @param  operation Opération (exemple "valeur" ou "sobel"). Ces opérations
+     *         correspondent à des noms des colonnes de la table "Environnement".
+     *         La liste des opérations disponibles peut être obtenu avec {@link
+     *         #getAvailableOperations()}.
+     * @param  parameter Paramètre (exemple "SST" ou "EKP"). La liste des paramètres
+     *         disponibles peut être obtenu avec {@link #getAvailableParameters()}.
+     * @param  position Position position relative sur la ligne de pêche où l'on veut
+     *         les valeurs. Les principales valeurs permises sont {@link #START_POINT},
+     *         {@link #CENTER} et {@link #END_POINT}.
+     * @param  timeLag Décalage temporel entre la capture et le paramètre environnemental,
+     *         en nombre de jours.
+     *
      * @throws SQLException si l'accès à la base de données a échoué.
      */
-    public void setTimeLags(final int[] timeLags) throws SQLException;
+    public void addParameter(final String operation,
+                             final String parameter,
+                             final int    position,
+                             final int    timeLag) throws SQLException;
 
     /**
-     * Retourne les colonnes de titres pour cette table.
+     * Retire un paramètre à la sélection.
+     *
+     * @param  operation Opération (exemple "valeur" ou "sobel").
+     * @param  parameter Paramètre (exemple "SST" ou "EKP").
+     * @param  position Position position relative sur la ligne de pêche où l'on veut
+     *         les valeurs.
+     * @param  timeLag Décalage temporel entre la capture et le paramètre environnemental,
+     *         en nombre de jours.
+     *
+     * @throws SQLException si l'accès à la base de données a échoué.
      */
-    public String[] getColumnTitles() throws SQLException;
+    public void removeParameter(final String operation,
+                                final String parameter,
+                                final int    position,
+                                final int    timeLag) throws SQLException;
 
     /**
-     * Retourne toutes les données. La première colonne du {@link ResultSet} retourné
+     * Retourne les nom des colonnes pour cette table. Ces noms de colonnes sont identiques
+     * à ceux que retourne <code>getRowSet(null).getMetaData().getColumnLabel(...)</code>.
+     * Cette méthode permet toutefoit d'obtenir ces noms sans passer par la coûteuse création
+     * d'un objet {@link RowSet}.
+     *
+     * @return Les noms de colonnes.
+     * @throws SQLException si l'accès à la base de données a échoué.
+     */
+    public String[] getColumnLabels() throws SQLException;
+
+    /**
+     * Retourne toutes les données. La première colonne du {@link RowSet} retourné
      * contiendra le numéro identifiant les captures. Toutes les colonnes suivantes
      * contiendront les paramètres environnementaux. Le nombre total de colonnes est
      * égal à la longueur de la liste retournée par {@link #getColumnTitles}.
      *
+     * @param  progress Objet à utiliser pour informer des progrès de l'initialisation,
+     *         ou <code>null</code> si aucun.
      * @return Les données environnementales pour les captures.
      * @throws SQLException si l'interrogation de la base de données a échoué.
      */
-    public ResultSet getData() throws SQLException;
+    public RowSet getRowSet(final Progress progress) throws SQLException;
 
     /**
      * Affiche les enregistrements vers le flot spécifié.
