512667400e22e76eafbe6e8e1ee3681be54fbed3
remi_eve <remi_eve>
2003-04-24 14:00:32 +0000

    First commit

---
+++ fr/ird/science/astro/SatelliteRelativeAngle.java
@@ -0,0 +1,139 @@
+package fr.ird.science.astro;
+
+// Geotools.
+import org.geotools.ct.CoordinateTransformation;
+import org.geotools.ct.CoordinateTransformationFactory;
+import org.geotools.ct.CannotCreateTransformException;
+import org.geotools.ct.TransformException;
+import org.geotools.cs.CoordinateSystem;
+import org.geotools.cs.GeocentricCoordinateSystem;
+import org.geotools.cs.GeographicCoordinateSystem;
+import org.geotools.pt.CoordinatePoint;
+
+/**
+ * Calcul l'angle zénithal et l'angle de visé (ou angle d'acquisition) du satellite relatif 
+ * à une position géographique. <BR><BR>
+ *
+ * Le schéma ci-dessous présente l'angle de visé (angle <i>B</i>) et l'angle zénithal 
+ * (angle <i>A</i> + angle <i>B</i>) du satellite.<BR><BR>
+ *
+ * <IMG SRC="doc-files/compute_angle.png"><BR><BR>
+ *
+ * Note : L'ensemble des calculs sont basés sur le système de coordonnées géographique 
+ * WGS_84.
+ *
+ * @verison $Id$
+ * @author Remi Eve
+ */ 
+public final class SatelliteRelativeAngle
+{
+    /** Angle zenithal du satellite (A + B). */
+    private double zenithalAngle;
+    
+    /** Angle de visé du satellite (angle B). */
+    private double sensorAngle;
+    
+    /** Transformation du systeme géographique vers le systeme géocentric. */
+    private final CoordinateTransformation transformation;    
+        
+    /** Système de coordonnées géocentrique utilisé. */
+    final CoordinateSystem targetCS = GeocentricCoordinateSystem.DEFAULT;
+
+    /** Système de coordonnées géographique utilisé. */
+    final CoordinateSystem sourceCS = GeographicCoordinateSystem.WGS84;
+
+    
+    /**
+     * Construit un objet de type SatteliteRelativeAngle.
+     */
+    public SatelliteRelativeAngle() throws Exception
+    {
+        /* Construit la transformation du système de coordonnées géographiques vers le 
+           système géocentriques. */
+        try 
+        {
+            CoordinateTransformationFactory factory = CoordinateTransformationFactory.getDefault();
+            transformation = factory.createFromCoordinateSystems(sourceCS, targetCS);
+        }
+        catch (CannotCreateTransformException e)
+        {
+            throw new Exception(e);
+        }                                 
+    }    
+    
+    
+    /**
+     * Calcul l'angle zénithal du satellite et l'angle de visé du satellite par rapport 
+     * à un point de la surface du globe.
+     *
+     * @param satGeog    Position du satellite dans le système géographique WGS_84 (x,y,z).
+     * @param altitude   Altitude du satellite en mètre.
+     * @param ptGeog     Position du point  (ou de l'observateur) dans le système 
+     *                   géographique WGS_84 (x,y,z).
+     */
+    public void compute(final CoordinatePoint satGeog,
+                        final double          altitude,
+                        final CoordinatePoint ptGeog) throws Exception 
+    {
+        // Transformation des points dans le système géocentric.
+        final CoordinatePoint ptGeoc  = new CoordinatePoint(3),        
+                              satGeoc = new CoordinatePoint(3);        
+        transformation.getMathTransform().transform(satGeog, satGeoc);
+        transformation.getMathTransform().transform(ptGeog,  ptGeoc);
+
+        /* Calcul des rayons RN et RA correspondant aux normes des vecteurs respectifs 
+           ON et OA. "O" representant le centre du globe. */
+        double RN = 0,
+               RA = 0;
+        for (int d=0; d<targetCS.getDimension(); d++)
+        {
+            RA += ptGeoc.getOrdinate(d)*ptGeoc.getOrdinate(d);
+            RN += satGeoc.getOrdinate(d)*satGeoc.getOrdinate(d);
+        }
+        RA = Math.sqrt(RA);
+        RN = Math.sqrt(RN);
+        
+        /* Calcul de l'angle "a". "a" étant l'angle forme entre les vecteurs ON et OA.
+           ON.OA = R*R*cos a. */
+        double pScal = 0;
+        for (int d=0 ; d<targetCS.getDimension(); d++)
+            pScal += satGeoc.getOrdinate(d) * ptGeoc.getOrdinate(d);
+        final double a = Math.acos(Math.min(Math.max(pScal / (RA*RN), 0),1));
+        
+        /* Calcul de d1. */
+        final double d1 = RA*Math.cos(a);
+
+        /* Calcul de d2. */
+        final double d2 = RN - d1;
+
+        /* Calcul de d. */
+        final double d = d2 + altitude;        
+                
+        /* Calcul de d3. */
+        final double d3 = RA*Math.sin(a);
+        
+        /* Calcul de l'angle "b". */
+        sensorAngle = Math.toDegrees(Math.atan((d3 / d)));
+        
+        /* Calcul de l'angle zenital du satellite egal a "a" + "b". */
+        zenithalAngle = Math.toDegrees(a) + sensorAngle;
+    }    
+        
+    /**
+     * Retourne l'angle zenital du satellite.
+     * @return l'angle zenital du satellite.
+     */
+    public double getZenithalAngle()
+    {
+        return zenithalAngle;
+    }
+    
+    /**
+     * Retourne l'angle de visé du satellite.
+     * @return l'angle de visé du satellite.
+     */
+    public double getSensorAngle()
+    {
+        return sensorAngle;
+    }    
+}
---
+++ fr/ird/science/math/TransfertFunction.java
@@ -0,0 +1,83 @@
+package fr.ird.science.math;
+
+/**
+ * Cette fonction retourne des valeurs comprises dans l'intervalle [0..1]. Cette fonction
+ * est définie comme suit : 
+ * <UL>
+ *  <LI>Pour tout <i>x</i> appartenant à l'intervalle <i>]min..max[</i>, 
+ *  <i><i>f(x)</i> appartient à l'intervalle <i>]0..1[</i>.</LI>
+ *  <LI>Pour tout <i>x<= min, f(x)=0</i>.</LI>
+ *  <LI>Pour tout <i>x>=max, f(x)=1.</i>.</LI>
+ * </UL><BR><BR>
+ * 
+ * La fonction utilisée est de la forme : <BR><BR>
+ *
+ * <CENTER><i> f(x) = 1 / (1 + e(cx - a)) <i></CENTER><BR>
+ * <IMG SRC="doc-files/function.png"><BR><BR>
+ *
+ * @version $Id$
+ * @author Remi EVE
+ */
+public class TransfertFunction 
+{   
+    /**
+     * Pour obtenir une fonction de transfert, nous nous sommes basés sur la 
+     * fonction de transfert f(x) = 1 / (1+e(x)). Cette fonction de transfert
+     * permet d'obtenir des valeurs comprises dans l'intervalle [0..1]. 
+     *
+     * Avec cette fonction, f(7.6) = 0.9999999 et f(-7,6) = 0.000001
+     *                      f(7.7) = 1         et f(-7.7) = 0
+     *                      f(180) = 1         et f(-213) = 0
+     */        
+     private final double min,  // valeur minimum   => f(min) = 0.000000001 
+                          max;  // valeur maximum   => f(max) = 0.999999999 
+    
+     /** Indique si les bornes sont consideres */
+     private boolean isMinIncluded, // if (true), f(min) = 0 else f(min) = 0.000001
+                     isMaxIncluded; // if (true), f(max) = 1 else f(max) = 0.9999999
+     
+    /** Coefficients de notre fonction de transfert. */
+    private final double C,
+                         A;        
+    
+    
+    /**
+     * Constructeur.
+     *
+     * @param   min             Borne inférieur.
+     * @param   isMinIncluded   <i>true</i> si la borne inférieur est comprise et <i>false</i>
+     *                          sinon.
+     * @param   max             Borne supérieur.
+     * @param   isMaxIncluded   <i>true</i> si la borne supérieur est comprise et <i>false</i>
+     *                          sinon.
+     */
+    public TransfertFunction (final double min, final boolean isMinIncluded, 
+                              final double max, final boolean isMaxIncluded) 
+    {        
+        this.min = min;
+        this.max = max;
+        this.isMinIncluded = isMinIncluded;
+        this.isMaxIncluded = isMaxIncluded;        
+        final double t1 = 7.6, 
+                     t2 = -7.6, 
+                     t3 = min, 
+                     t4 = max;
+        C = (t2 - t1) / (t4 - t3);                
+        A = t1 - C*t3;        
+    }        
+
+    /**
+     * Retourne f(x) appartenant à l'intervalle [0..1].
+     *
+     * @param   x   Valeur à injecter dans la fonction.
+     * @return f(x) appartenant à l'intervalle [0..1].
+     */
+    public double compute(final double x)     
+    {
+        if ((x > max) || ((x == max) && (isMaxIncluded)))
+            return 1.0;
+        else if ((x < min) || ((x == min) && (isMinIncluded)))
+            return 0.0;
+        return (1.0/(1.0 + Math.exp(C*x+A)));
+    }
+}
