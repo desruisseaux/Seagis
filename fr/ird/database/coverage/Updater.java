75b161e7d37f916a193a85439486cf25b52858a4
remi_eve <remi_eve>
2003-12-22 16:08:46 +0000

    *** empty log message ***

--- fr/ird/database/coverage/Updater.java
+++ fr/ird/database/coverage/Updater.java
@@ -28,11 +28,9 @@ import java.util.StringTokenizer;
 import java.util.Vector;
 
 // SEAGIS.
-//import fr.ird.resources.Utilities;
 import fr.ird.database.coverage.CoverageEntry;
 import fr.ird.database.coverage.CoverageTable;
 import fr.ird.database.coverage.sql.CoverageDataBase;
-import fr.ird.io.text.ParseHeader;
 
 // GEOTOOLS.
 import org.geotools.gc.GridCoverage;
@@ -51,177 +49,7 @@ import org.geotools.resources.CTSUtilities;
  * @version $Id$
  */
 public class Updater {
-    /**
-     * First column  : begin of file name containing a product.
-     * Second column : serie to add file name begin with first column.
-     * Third column  : begin of file name inserted to databse Image.
-     */
-    private final static int PREFIX_SRC        = 0,
-                             FORMAT_DATE_SRC   = 1,
-                             SERIE             = 2,
-                             OFFSET_START_TIME = 3,
-                             OFFSET_END_TIME   = 4,
-                             PREFIX_TGT        = 5,
-                             FORMAT_DATE_TGT   = 6;
-    private static final String[][] PARAMETERS = getParameters();
-                                                           
-    /** Chemin par défaut du fichier de configuration. */
-    private final static String DEFAULT_FILE = "application-data/configuration/UpdateDatabase.txt";
-                                             
-                                             
-    /**
-     * Extract parameter for inserting image in databse 'Images'.
-     */                                             
-    private static final String[][] getParameters() {  
-        try {
-            final int size = 7; // Number of parameters by line.
-            final BufferedReader input = new BufferedReader(new FileReader(new File(Updater.class.getClassLoader().getResource(DEFAULT_FILE).getPath())));
-
-            // Listes des paramétres.
-            final Vector vParameters = new Vector();
-
-            // Parcours ligne à ligne.
-            String line;
-            int count = 0;
-            while ((line=input.readLine())!=null) {
-                count ++;
-                // Ligne vide ou commentaire.
-                line = line.trim();            
-                if (line.length() == 0 ||line.startsWith("#")) {
-                    continue;
-                }
-
-                final StringTokenizer token = new StringTokenizer(line);                        
-                final int length = token.countTokens();
-                if (length < size) {
-                    System.err.println("An error occurs in file '" + DEFAULT_FILE + "' at line " + count + ".");
-                }
-
-                final String[] parameter = new String[size];
-                for (int i=0 ; i<size && token.hasMoreTokens(); i++) {
-                    parameter[i] = token.nextToken();
-                }
-
-                vParameters.add(parameter);
-            }
-
-            final String[][] parameters = new String[vParameters.size()][];
-            for (int i=0 ; i<vParameters.size() ; i++) {
-                parameters[i] = (String[])vParameters.get(i);
-            }
-            return parameters;
-        }
-        catch (IOException e) {
-            System.err.println(e.getMessage());
-            return null;
-        }
-    }
-                                             
-    /**
-     * Insert a new image to database from filename of image and his 
-     * associated header.
-     *
-     * @param src       Name of Image source.
-     * @param header    Name of file header associated to image.
-     */
-    public static void insertToDataBase(final File src, final File header) throws IOException, SQLException
-    {
-        if (PARAMETERS == null) {
-            throw new IllegalStateException("Can't determine target serie for the image '" + src + "'.");            
-        }
         
-        //////////////////////////
-        // search target serie. //
-        //////////////////////////
-        final String nameSrc = src.getName();
-        boolean find = false;
-        int index = -1;
-        
-        for (int i=0 ; i<PARAMETERS.length ; i++) 
-        {
-            // Compare with pattern.            
-            final String pattern = PARAMETERS[i][PREFIX_SRC] + "[0-9]{" + PARAMETERS[i][FORMAT_DATE_SRC].length() + "}.*";            
-            if (Pattern.matches(pattern, nameSrc)) {
-                if (find == false) {
-                    find = true;
-                    index = i;
-                } else {
-                    throw new IllegalStateException("Find more than one target series for image '" + src + "'.");
-                }
-            }
-        }
-        
-        if (find == false) {
-            throw new IllegalStateException("Can't determine target serie for the image '" + src + "'.");
-        }
-                
-        /////////////////////////////////////////////////
-        // Compute start and end time of this product. //
-        /////////////////////////////////////////////////
-        final Date dateSrc   = getDate(src, PARAMETERS[index][PREFIX_SRC], PARAMETERS[index][FORMAT_DATE_SRC]);
-        final Date startTime = new Date(dateSrc.getTime() - Long.parseLong(PARAMETERS[index][OFFSET_START_TIME])),
-                   endTime   = new Date(dateSrc.getTime() + Long.parseLong(PARAMETERS[index][OFFSET_END_TIME]));
-        
-        ////////////////////////
-        // Build target name. //
-        ////////////////////////
-        final String tgt = getTargetFile(endTime, PARAMETERS[index][PREFIX_TGT], PARAMETERS[index][FORMAT_DATE_TGT]);
-        
-        ///////////////////////
-        // Extract envelope. //
-        ///////////////////////
-        final ParameterList parameterIn = ParseHeader.getInputDefaultParameterList();
-        parameterIn.setParameter(ParseHeader.FILE, header);
-        final ParameterList parameterOut = ParseHeader.parse(parameterIn);
-        final Point2D origine = (Point2D)parameterOut.getObjectParameter(ParseHeader.ORIGINE);
-        final int width  = parameterOut.getIntParameter(ParseHeader.WIDTH),
-                  height = parameterOut.getIntParameter(ParseHeader.HEIGHT);
-        final double[] resolution = (double[])parameterOut.getObjectParameter(ParseHeader.RESOLUTION);        
-        final Rectangle2D area = new Rectangle2D.Double(origine.getX(), 
-                                                        origine.getY() - resolution[1] * height,
-                                                        width  * resolution[0],
-                                                        height * resolution[1]);
-        
-        ///////////////////////////
-        // Inserting to database. //
-        ///////////////////////////
-        final CoverageDataBase database = new CoverageDataBase();
-        final String serie = database.getCoverageTable(Integer.parseInt(PARAMETERS[index][SERIE])).getSeries().getName();
-        insertToDataBase( database, src, tgt, serie, startTime, endTime, area);
-    }
-    
-    /**
-     * @return target file name.
-     * @parma date      Date of product.
-     * @param prefix    Target file name prefix.
-     * @param format    Date format.
-     * @return target file name.
-     */
-    private final static String getTargetFile(final Date date, final String prefix, final String format) 
-    {
-        final SimpleDateFormat dateFormat = new SimpleDateFormat(format);
-        final StringBuffer buffer = new StringBuffer();
-        return prefix + dateFormat.format(date, buffer, new FieldPosition(0)).toString();
-    }
-    
-    /**
-     * Return date from file source.
-     * @return date from file source.
-     */
-    private final static Date getDate(final File src, final String prefix, final String format) 
-    {
-        final SimpleDateFormat dateFormat = new SimpleDateFormat(format);
-        dateFormat.setTimeZone(TimeZone.getTimeZone("UTC"));
-        final String date  = src.getName().toString().substring(prefix.length(), prefix.length() + format.length());
-        final Date dateSrc = dateFormat.parse(date, new ParsePosition(0));
-        
-        final Calendar calendar = new GregorianCalendar(TimeZone.getTimeZone("UTC"));
-        calendar.clear();
-        calendar.setTime(dateSrc);
-        return calendar.getTime();
-    }
-    
-    
     //////////////////////////////////////////////////////////////////////////////////////
     /////////////                                                          ///////////////    
     /////////////                           PRIVATE                        ///////////////
@@ -359,27 +187,4 @@ public class Updater {
         
         return new Envelope(min, max);
     }    
- 
-    
-    //////////////////////////////////////////////////////////////////////////////////////
-    /////////////                                                          ///////////////    
-    /////////////                               MAIN                       ///////////////
-    /////////////                                                          ///////////////    
-    //////////////////////////////////////////////////////////////////////////////////////
-    /**
-     * Main.
-     */
-    public static void main(final String[] args) 
-        throws IOException, SQLException
-    {
-        final int count = args.length;
-        if (count != 1)  {
-            System.err.println("Insert a new image into database");
-            System.err.println("Update IMAGE");
-            System.err.println("IMAGE                   --> Nom du fichier représentant l'image à insérer.");
-            System.exit(-1);
-        }
-        
-        Updater.insertToDataBase(new File(args[0]), new File(args[0] + ".hdr"));
-    }
 }
